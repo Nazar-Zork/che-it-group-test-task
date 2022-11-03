4be7f132ee07ed5d699f281109195aecdbdd793b
Nazar Kharchuk <huntercat2785@gmail.com>
2022-11-03 11:41:00 +0200

    last fixed

--- js/accordion-anim.js
+++ js/accordion-anim.js
@@ -12,18 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	accordions.forEach(el => {
 		el.addEventListener('click', (e) => {
 			const self = e.currentTarget;
-			const control = self.querySelector('.faq__accordionControl');
 			const content = self.querySelector('.faq__accordionContent');
 
 			self.classList.toggle('open');
 
 			if (self.classList.contains('open')) {
-				control.setAttribute('aria-expanded', true);
-				content.setAttribute('aria-hidden', false);
 				content.style.maxHeight = 400 + 'px';
 			} else {
-				control.setAttribute('aria-expanded', false);
-				content.setAttribute('aria-hidden', true);
 				content.style.maxHeight = null;
 			}
 		});
